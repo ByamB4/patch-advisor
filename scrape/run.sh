d76a0749af44a43363e2f25a2a7c4edd739ea771
ByamB4 <byamba4life@gmail.com>
2024-06-04 16:30:49 +0800

    [core] added few newline

--- scrape/run.sh
+++ scrape/run.sh
@@ -1,8 +1,9 @@
-# docker volume create scrape-data
-# docker build -t ubuntu -f Dockerfile .
+docker volume create scrape-data
+docker build -t ubuntu -f Dockerfile .
 
 # method 1: single command
-  # docker run --rm -it -v $(pwd)/scrape:/home -e PYTHONBUFFERED=1 ubuntu python3 /home/redhat.py
+  docker run --rm -it -v $(pwd)/scrape:/home -e PYTHONBUFFERED=1 ubuntu python3 /home/redhat.py
 
 # method 2: inside box
-  # docker run --rm -it -v $(pwd)/scrape:/home ubuntu /bin/bash
+  docker run --rm -it -v $(pwd)/scrape:/home ubuntu /bin/bash
+
