26c3f4d084bc6fd11f464212caa50d8fe181e9e4
Byambadalai Sumiya <byamba4life@gmail.com>
2024-06-12 11:36:02 +0800

    Update vmware.sh

--- scrape/vmware.sh
+++ scrape/vmware.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# docker run --rm -v /root/patch-advisor/scrape:/home ubuntu-scrape python3 /home/vmware.py >/tmp/patch/vmware/scrape.suc.$(date +\%d.\%H.\%M) 2>/tmp/patch/vmware/scrape.err.$(date +\%d.\%H.\%M)
 docker run --rm -v /root/patch-advisor/scrape:/home ubuntu-scrape python3 /home/vmware.py
