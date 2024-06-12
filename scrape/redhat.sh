53cc776132b0e999bfcacd91a0eb619884db5334
Byambadalai Sumiya <byamba4life@gmail.com>
2024-06-12 11:35:45 +0800

    Update redhat.sh

--- scrape/redhat.sh
+++ scrape/redhat.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-# docker run --rm -v /root/patch-advisor/scrape:/home ubuntu-scrape python3 /home/redhat.py >/tmp/patch/redhat/scrape.suc 2>/tmp/patch/redhat/scrape.err
-docker run --rm -v /home/byamb4/dev/patch-advisor/scrape:/home ubuntu-scrape python3 /home/redhat.py
+docker run --rm -v /root/patch-advisor/scrape:/home ubuntu-scrape python3 /home/redhat.py >/tmp/patch/redhat/scrape.suc 2>/tmp/patch/redhat/scrape.err
